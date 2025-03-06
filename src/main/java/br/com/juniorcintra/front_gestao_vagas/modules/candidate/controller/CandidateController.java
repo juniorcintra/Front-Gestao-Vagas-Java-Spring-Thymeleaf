12699e01b2e4005605022af7133771edf554fdb3
juniorcintra <junior.cintra.developer@gmail.com>
2025-03-06 11:54:16 -0300

    RestTemplate para acesso a API externa

--- src/main/java/br/com/juniorcintra/front_gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/juniorcintra/front_gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,16 +1,35 @@
 package br.com.juniorcintra.front_gestao_vagas.modules.candidate.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import br.com.juniorcintra.front_gestao_vagas.modules.candidate.service.CandidateService;
 
 @Controller
 @RequestMapping("/candidate")
 public class CandidateController {
 
+  @Autowired
+  private CandidateService candidateService;
+
   @GetMapping("/login")
-  public String login() {
+  public String loginPage() {
     return "candidate/login";
   }
 
+  @PostMapping("/login")
+  public String login(RedirectAttributes redirectAttributes, String username, String password) {
+    try {
+      this.candidateService.login(username, password);
+
+      return "candidate/profile";
+    } catch (HttpClientErrorException e) {
+      redirectAttributes.addFlashAttribute("error_message", "Usuário ou senha inválidos");
+      return "redirect:/candidate/login";
+    }
+  }
 }
---
+++ src/main/java/br/com/juniorcintra/front_gestao_vagas/modules/candidate/dto/Token.java
@@ -0,0 +1,15 @@
+package br.com.juniorcintra.front_gestao_vagas.modules.candidate.dto;
+
+public class Token {
+
+  private String access_token;
+
+  public String getAccess_Token() {
+    return access_token;
+  }
+
+  public void setAccess_Token(String access_token) {
+    this.access_token = access_token;
+  }
+
+}
